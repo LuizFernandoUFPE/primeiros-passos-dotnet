93ae77f1a7ab9a6d106479230ce925f4f21f60c4
Luiz Fernando da Silva Santos <luiz.peony@gmail.com>
2022-01-06 02:15:41 -0300

    Adição dos arquivos do treinamento de primeiros passos com .NET da DIO

---
+++ 00.ProjectFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectFirst
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Hello, World!" );
+
+        int numeroDeVezes = 20;
+
+        for(int i = 0; i < numeroDeVezes; i++)
+        {
+            Console.WriteLine($"Bem-vindo ao meu treino da DIO {i}");
+        }
+        }   
+    }
+}   
+
+
---
+++ 01.EstruturaDoPrograma/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            s.Empilha(5); //Testei um novo número para observar a pilha e a propriedade de last-in First-Out(LIFO)
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
